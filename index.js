6cc9eb82bbb9011ba7642d451534be3329be7bb6
Rohit Vishwakarma <rohit8840@gmail.com>
2024-06-22 16:40:33 +0530

    code insertion

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser=require('body-parser');
 const app=express();
 
 const PORT=3000;
+
+// Middleware for parsing the body so that we can read the data that we send through post type 
+// request in that 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 
 // Mimic the database using the array 
@@ -12,8 +18,19 @@ app.get('/blogs',(req,res)=>{
 
     return res.status(200).json({
     data: blogList,
-    success: true
+    success: true,
     });
+});
+
+
+app.post('/blogs',(req,res)=>{
+    // console.log(req.body);
+    // console.log(req.query);
+blogList.push({title :req.body.title, content: req.body.content});
+return res.status(201).json({
+    success: true,
+})
+
 });
 
 app.listen(PORT,()=>{
