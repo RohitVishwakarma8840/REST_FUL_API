aa982d75dddcddc3e5f8c4c10696a41380322fa4
Rohit Vishwakarma <rohit8840@gmail.com>
2024-06-22 19:19:06 +0530

    code insertion

--- index.js
+++ index.js
@@ -14,9 +14,13 @@ app.use(bodyParser.json());
 
 let blogList=[];
 
+// app.get('/getall',(req,res)=>{
+//     res.send(blogList);
+// });
+
 app.get('/blogs',(req,res)=>{
 
-    return res.status(200).json({
+    return res.status(404).json({
     data: blogList,
     success: true,
     });
@@ -26,12 +30,48 @@ app.get('/blogs',(req,res)=>{
 app.post('/blogs',(req,res)=>{
     // console.log(req.body);
     // console.log(req.query);
-blogList.push({title :req.body.title, content: req.body.content});
+blogList.push({title :req.body.title,
+     content: req.body.content,
+     id:Math.floor(Math.random()*1000)
+     
+    });
 return res.status(201).json({
     success: true,
 })
 
 });
+
+
+app.get('/blogs/:id',(req,res)=>{
+    // console.log(req.params);
+ const result= blogList.filter((blog)=>blog.id ==req.params.id);
+ return res.status(200).json({
+    data : result,
+    id: req.params.id,
+    success : true
+ })
+
+})
+
+// app.delete('/blogs/:id', (req, res) => {
+//     const blogId = parseInt(req.params.id, 10);
+//     // Find index of blog in array
+//     const index = blogList.findIndex(b => b.id === blogId);
+//     if (index !== -1) {
+//         // Remove blog from array
+//         blogList.splice(index, 1);
+//         return res.status(200).json({
+//             message: 'Blog deleted successfully',
+//             success: true
+//         });
+//     } else {
+//         return res.status(404).json({
+//             message: 'Blog not found',
+//             success: false
+//         });
+//     }
+// });
+
 
 app.listen(PORT,()=>{
     console.log("Server Started on PORT", PORT);
