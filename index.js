53f2fd45f73f37853502de7cc09f0f5740db6a42
Rohit Vishwakarma <rohit8840@gmail.com>
2024-06-26 11:10:36 +0530

    code insertion

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const PORT=3000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+// Middleware 
+app.use(logger);
+// you can say logger is a global Middleware 
 
 // Mimic the database using the array 
 
@@ -18,6 +21,15 @@ let blogList=[];
 function logger(req,res,next){
   console.log(req.url);
   console.log(req.body);
+
+ let Condition = true;
+  if(Condition){
+
+   return res.status(500).json({
+    message :"Something went wrong"
+   })
+
+  }
   next();
 
 }
@@ -32,7 +44,9 @@ function isAuthenticated(req,res,next){
 //     res.send(blogList);
 // });
 
-app.get('/blogs',logger,isAuthenticated , (req,res)=>{
+// Authenticated is a Middleware that is used when called by app.get unlinke app.get it will not 
+
+app.get('/blogs',isAuthenticated , (req,res)=>{
     
    console.log("hitting");
     return res.status(404).json({
